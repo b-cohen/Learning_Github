09e82c60c04cabff5944ada88541526c0d7bf9e1
Jordan <jdonahue24@basecampcodingacademt.org>
2023-08-14 15:49:07 -0500

    Jodan was here

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-#This is a file for the class roster.
-#Matthew Hays
-#Brittany Cohen
+# This is a file for the class roster.
+# Matthew Hays
+# Brittany Cohen
 # This is a file for the class roster.
 # Brittany Cohen
 # Ab
 # Blaine
 # Kaleigh
+# Jordan
