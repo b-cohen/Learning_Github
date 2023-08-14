079945d7898769e7a6798d571859b6fe2801d16e
Caleb <chorn24@basecampcodingacademy.org>
2023-08-14 15:40:00 -0500

    WORK!

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 # This is a file for the class roster.
 # Caleb Horn
-=======
-#This is a file for the class roster.
-#Brittany Cohen
->>>>>>> ccb062f672d314c538cc9ae99645e46379917bcf
+# This is a file for the class roster.
+# Brittany Cohen
