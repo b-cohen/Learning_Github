72ced2333628a9ee8f8f490743b6279f567c728d
mhays24 <142264785+mhays24@users.noreply.github.com>
2023-08-14 16:08:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-# jeremiah
 #This is a file for the class roster.
 #yan mori
-#Matthew Hays
 #Brittany Cohen
-# This is a file for the class roster.
 # Abigail Usener
-# Matthew Hays
-# Brittany Cohen
-# This is a file for the class roster.
-# Brittany Cohen
 # Joby Foster
 # Matthew Hays
-# MatthewHays24
-# Brittany Cohen
-# This is a file for the class roster.
-# Brittany Cohen
 # RJay Pickering
 # Ab
 # Blaine
 # Kaleigh
-<<<<<<< HEAD
-
-
-=======
 # Jordan
